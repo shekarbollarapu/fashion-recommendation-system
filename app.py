80cf9528842224ec219333ba0d14311ea5014307
SHEKAR BOLLARAPU <167456780+shekarbollarapu@users.noreply.github.com>
2024-12-13 00:04:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,42 +5,39 @@ from tensorflow.keras.models import Sequential
 import numpy as np
 from numpy.linalg import norm
 import os
-import joblib
 from tqdm import tqdm
+import pickle
 
-# Initialize ResNet50 model
 model = ResNet50(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
 model.trainable = False
-model = Sequential([model, GlobalMaxPooling2D()])
 
-# Function to extract features
-def extract_features(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
+model = Sequential([model, GlobalMaxPooling2D()])
+#model.summary()
+
+def extract_features(img_path,model):
+    img = image.load_img(img_path,target_size=(224,224))
     img_array = image.img_to_array(img)
-    expand_img = np.expand_dims(img_array, axis=0)
+    expand_img = np.expand_dims(img_array,axis=0)
     preprocessed_img = preprocess_input(expand_img)
     result_to_resnet = model.predict(preprocessed_img)
     flatten_result = result_to_resnet.flatten()
+    # normalizing
     result_normlized = flatten_result / norm(flatten_result)
-    return result_normlized
 
-# List of image paths
+    return result_normlized
+#print(os.listdir('fashion_small/images'))
 img_files = []
 
-# Assuming the fashion images are in the 'fashion_small/images' directory
 for fashion_images in os.listdir('fashion_small/images'):
     images_path = os.path.join('fashion_small/images', fashion_images)
     img_files.append(images_path)
 
-# Extracting image features
+# extracting image features
 image_features = []
 
 for files in tqdm(img_files):
     features_list = extract_features(files, model)
     image_features.append(features_list)
 
-# Save the features and file list using joblib
-joblib.dump(image_features, 'image_features_embedding.joblib')
-joblib.dump(img_files, 'img_files.joblib')
-
-print("Feature extraction and saving completed!")
+pickle.dump(image_features, open("image_features_embedding.pkl", "wb"))
+pickle.dump(img_files, open("img_files.pkl", "wb"))
